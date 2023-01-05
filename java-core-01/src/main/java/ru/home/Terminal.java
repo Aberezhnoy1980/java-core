9cc9c6d89e9630e6934320ef1b8dccc67f28bbd0
Aleksandr <Berezhnoy_aa@mail.ru>
2023-01-05 11:29:53 +0300

    new data

--- java-core-01/src/main/java/ru/home/Terminal.java
+++ java-core-01/src/main/java/ru/home/Terminal.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Terminal {
     public static void main(String[] args) {
-        System.out.print("What's your name? ");
+        System.out.println("What's your name? ");
         String name = new Scanner(System.in).nextLine();
         System.out.println("Hello " + name + "!");
     }
